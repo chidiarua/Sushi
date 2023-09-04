b82befdfdfe6359616f6fa55b4c4e7cc3d0b6794
chidiarua <chidiarua99@gmail.com>
2023-09-04 17:49:24 +0100

    v1.1 explore button

---
+++ index.js
@@ -0,0 +1,11 @@
+const exploreBtn = document.querySelector('.explore');
+
+//const exploreBtnIcon = document.querySelector('.explore img');
+
+const exploreDropMenu = document.querySelector('.explore_menu_items');
+
+
+function showExploreMenu(){
+    exploreDropMenu.classList.toggle('open');
+
+}
